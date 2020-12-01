302b14270c8a249c7d1174200627de20025c09e8
Starcush <hacker0419@gmail.com>
2020-12-01 17:46:02 +0900

    eslint설치 및 설정
    package.json 파일에도 eslint 적용
    
    eslint 설정에 맞게 prettier 설정도 변경

---
+++ slack-clone-client/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
