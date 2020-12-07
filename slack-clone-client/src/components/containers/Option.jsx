ca875dea6928f67e11003a5962ff260ef04f4791
Starcush <hacker0419@gmail.com>
2020-12-07 19:20:36 +0900

    option div 영역 기본 UI 마무리
    profile 추가

--- slack-clone-client/src/components/containers/Option.jsx
+++ slack-clone-client/src/components/containers/Option.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
-/* eslint-disable */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faArrowRight, faUser } from '@fortawesome/free-solid-svg-icons';
 
 import {
   OptionDiv,
@@ -11,10 +10,19 @@ import {
   OptionInput,
   LeftArrowDiv,
   RightArrowDiv,
+  ProfileContainer,
+  ProfileDiv,
 } from '../views/StyledComponents';
 import * as style from '../views/styledVariables';
 
-const Option = (props) => {
+// option 영역에 프로필 div를 추가할 예정
+// 로그인이 되지 않았을 때와
+// 로그인이 됐을 때를 구분해서 아이콘이 나올 수 있도록
+// 가져다 대면 내 닉네임이 대화상자처럼 나오고
+// 클릭은 못한다
+// 하지만 로그인이 안되있는 상태면 로그인이 가능한 팝업 창이 뜬다 => 이건 구글 소셜 로그인으로
+
+const Option = () => {
   const [arrowLeft, setArrowLeft] = React.useState(false);
   const [arrowRight, setArrowRight] = React.useState(false);
 
@@ -47,6 +55,11 @@ const Option = (props) => {
       <OptionSearchDiv>
         <OptionInput borderColor={style.lightPurple} type="text" />
       </OptionSearchDiv>
+      <ProfileContainer>
+        <ProfileDiv>
+          <FontAwesomeIcon icon={faUser} style={{ color: '#0074D9' }} />
+        </ProfileDiv>
+      </ProfileContainer>
     </OptionDiv>
   );
 
@@ -68,8 +81,8 @@ const Option = (props) => {
 
   function mouseLeaveEvent() {
     console.log('mouse leave');
-    if(arrowRight) setArrowRight(!arrowRight);
-    if(arrowLeft) setArrowLeft(!arrowLeft);
+    if (arrowRight) setArrowRight(!arrowRight);
+    if (arrowLeft) setArrowLeft(!arrowLeft);
   }
 };
 
